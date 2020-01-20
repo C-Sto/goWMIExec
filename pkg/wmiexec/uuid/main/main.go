56588e5af63e7f6ff8dc037e2d1b832db9ae2479
C-Sto <7466346+C-Sto@users.noreply.github.com>
2020-01-20 17:06:14 +0800

    better uuids for less confusion

---
+++ pkg/wmiexec/uuid/main/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/C-Sto/goWMIExec/pkg/wmiexec/uuid"
+)
+
+func main() {
+	f := flag.String("test", "hexhexhex", "hex")
+	flag.Parse()
+	s := strings.ReplaceAll(*f, ",", "")
+	s = strings.ReplaceAll(s, "0x", "")
+	s = strings.ReplaceAll(s, " ", "")
+	b, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(uuid.FromBytes(b))
+}
